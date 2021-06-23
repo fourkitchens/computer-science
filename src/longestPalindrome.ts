31ae4bb4254d56e7141c379eb3f027617f25dacb
Patrick Coffey <patrickcoffey48@gmail.com>
2021-06-23 17:21:58 -0500

    feat: add longestPalindrome solution

--- src/longestPalindrome.ts
+++ src/longestPalindrome.ts
@@ -1,3 +1,33 @@
 export function longestPalindrome(input: string): string {
-  return input;
+  if (input.length <= 1) {
+    return input;
+  }
+
+  let longest = '';
+  for (let i = 0; i < input.length; i++) {
+    if (longest.length > input.length - i) {
+      break;
+    }
+
+    const val =
+      input.length % 2 === 0
+        ? expandAroundCenter(input, i - 1, i)
+        : expandAroundCenter(input, i, i);
+
+    if (val.length > longest.length) {
+      longest = val;
+    }
+  }
+
+  return longest;
+}
+
+function expandAroundCenter(s: string, left: number, right: number): string {
+  let i = 0;
+  while (s[left - i] && s[left - i] === s[right + i]) {
+    i++;
+  }
+  i--;
+
+  return s.slice(left - i, right + i + 1);
 }
